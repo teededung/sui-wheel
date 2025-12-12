849ae938fba63d1faf621c34cfc6c4f66ca091b6
teededung <start@hotmail.com.vn>
2025-12-12 16:43:35 +0700

    cleanup

--- supabase/migrations/20251027041010_fix_function_search_path.sql
+++
@@ -1,5 +0,0 @@
--- Fix function search path
--- This migration ensures proper search path for database functions
-
--- Set search path for public schema
-ALTER DATABASE postgres SET search_path TO public, extensions;
--- supabase/migrations/20251116160000_remove_entries_synced.sql
+++
@@ -1,3 +0,0 @@
--- Remove entries_synced column as auto-sync feature has been removed
-ALTER TABLE public.wheels 
-DROP COLUMN IF EXISTS entries_synced;
